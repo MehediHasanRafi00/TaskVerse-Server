660291002069e0285b65d878fadb94b2b0bbe40f
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2025-11-09 03:06:01 +0600

    Configure MongoDB connection with Atlas

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(JSON())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running fine!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@desmondhart.tz893ku.mongodb.net/?appName=Desmondhart`;
 
-app.get('/',(req,res)=>{
-  res.send('Server is running fine!')
-})
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("TaskVerseDB");
+    const jobColl = db.collection("jobs");
+
+    app.get("/allJobs", async (req, res) => {
+      const result = await jobColl.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+  console.log(`Server is listening on port ${port}`);
+});
